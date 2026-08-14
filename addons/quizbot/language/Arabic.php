9ded90130ecef2ccb01cc80bf74ac1c4462cda83
hearts-byte <tlaqywtalt@gmail.com>
2026-08-15 01:23:52 +0300

    Add files via upload

---
+++ addons/quizbot/language/Arabic.php
@@ -0,0 +1,26 @@
+<?php
+// quizbot language
+
+$lang['sorry'] = 'عذراً، لم يجد أحد الإجابة : %answer% ... حظاً أوفر في السؤال القادم.';
+$lang['good'] = '%wsign% تهانينا %user% وجد الإجابة : %answer% وربح %score% ليصبح مجموعه %tscore%.';
+$lang['question'] = '%qsign% %question% : %words% كلمة/كلمات';
+$lang['scramble'] = '%psign% كلمات متقاطعة : %question%';
+$lang['hint'] = '%hsign% تلميح : %answer%';
+$lang['points'] = '%points% نقطة';
+$lang['quiz_leaderboard'] = 'نتائج المسابقة';
+
+// quizbot config
+
+$lang['name'] = 'اسم بوت المسابقة';
+$lang['room'] = 'غرفة المسابقة';
+$lang['reset'] = 'إعادة تعيين النتائج';
+$lang['status'] = 'حالة المسابقة';
+$lang['quiz_file'] = 'ملف المسابقة';
+$lang['scramble_file'] = 'ملف الكلمات المبعثرة';
+$lang['quiz_type'] = 'نوع المسابقة';
+$lang['scramble_mode'] = 'كلمات مبعثرة';
+$lang['quiz_mode'] = 'مسابقة';
+$lang['mix_mode'] = 'عشوائي';
+$lang['reset_score'] = 'إعادة تعيين النقاط';
+$lang['want_reset_quiz'] = 'هل أنت متأكد أنك تريد إعادة تعيين نتائج المسابقة؟ هذا الإجراء لا يمكن التراجع عنه';
+?>
