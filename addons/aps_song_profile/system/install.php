43a7bc6a751dee80ecf096c75bdea2ea69238856
hearts-byte <tlaqywtalt@gmail.com>
2026-08-15 23:04:22 +0300

    Add files via upload

--- addons/aps_song_profile/system/install.php
+++ addons/aps_song_profile/system/install.php
@@ -3,11 +3,7 @@ if(!defined('BOOM')){
 	die();
 }
 
-$ad = array(
-	'name' => 'aps_song_profile',
-	'access'=> 1,
-	'custom1'=> 5,
-);
+$mysqli->query("INSERT INTO `boom_addons` (`addons`, `addons_load`, `addons_access`, `custom1`) VALUES ('aps_song_profile', '1', '1', '5')");
 
 $mysqli->query("ALTER TABLE `boom_users` ADD profile_music varchar(200) NOT NULL DEFAULT ''");
 ?>
