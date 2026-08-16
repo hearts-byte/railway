11cce2e042404361fa9808c6f25ce98a7fb5d575
hearts-byte <tlaqywtalt@gmail.com>
2026-08-16 22:37:44 +0300

    Add blk_emoticons addon

---
+++ addons/blk_emoticons/language/Default.php
@@ -0,0 +1,14 @@
+<?php
+$lang['blk_emoticons_title'] = 'BLK Emoticons';
+$lang['blk_categories_label'] = 'فئات الإيموجي (الباقات)';
+$lang['blk_add_category'] = 'إضافة فئة جديدة';
+$lang['blk_category_name'] = 'اسم الفئة بالإنجليزي (مثال: custom1 أو sticker1)';
+$lang['blk_category_icon'] = 'أيقونة الفئة اللي تظهر بقائمة التبويبات (png / svg / gif) - اختياري';
+$lang['blk_add_btn'] = 'إضافة الفئة';
+$lang['blk_upload_btn'] = 'رفع إيموجي';
+$lang['blk_manage_hint'] = 'اضغط على صورة الإيموجي لتعديل كوده (اسمه)، وعلى علامة X لحذفه.';
+$lang['blk_no_emo'] = 'لا توجد إيموجيات بهذه الفئة بعد.';
+$lang['blk_base_category'] = 'الأساسية';
+$lang['blk_confirm_delete_cat'] = 'حذف هذه الفئة سيحذف كل الإيموجيات اللي بداخلها, متأكد؟';
+$lang['blk_new_code_prompt'] = 'كود الإيموجي الجديد (بدون النقطتين):';
+?>
