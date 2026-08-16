d975dc982036d9146864f9126889a91d37ca51ba
hearts-byte <tlaqywtalt@gmail.com>
2026-08-17 00:38:42 +0300

    Delete addons/bet_gold directory

--- addons/bet_gold/language/Default.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lang['titlemodclo'] = 'Addons By Dedar';
-$lang['settings'] = 'settings';
-$lang['limit_feature'] = 'limit feature';
-$lang['save'] = 'Save';
-$lang['TitleTimeMenu'] = 'Addons By Dedar';
-?>
--- addons/bet_gold/system/uninstall.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if(!defined('BOOM')){
-	die();
-}
-?>
