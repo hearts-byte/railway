bec303249ed2e04a619e78b91d3b226f705ce7b8
hearts-byte <tlaqywtalt@gmail.com>
2026-08-17 22:41:50 +0300

    Add files via upload

--- system/box/admin_edit_rank.php
+++ system/box/admin_edit_rank.php
@@ -15,4 +15,10 @@ if(!canRankUser($user)){
 	<select id="profile_rank" onchange="changeRank(this, <?php echo $user['user_id']; ?>);">
 		<?php echo changeRank($user); ?>
 	</select>
+	<?php if(useLevel()){ ?>
+	<div class="setting_element">
+		<p class="label"><?php echo $lang['user_level']; ?>: <?php echo $user['user_level']; ?></p>
+		<button type="button" class="reg_button theme_btn" onclick="levelUpUser(<?php echo $user['user_id']; ?>);"><?php echo $lang['level_up']; ?></button>
+	</div>
+	<?php } ?>
 </div>
