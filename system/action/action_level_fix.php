1ca7f2c24df01dc98c7ec2e1c336921919088bf2
hearts-byte <tlaqywtalt@gmail.com>
2026-08-18 01:21:51 +0300

    Add files via upload

--- system/action/action_level_fix.php
+++ system/action/action_level_fix.php
@@ -1,11 +1,18 @@
 <?php
 require('../config_session.php');
 
-if(!isset($_POST['level_up'])){
+if(!isset($_POST['level_up']) || !isset($_POST['amount'])){
 	die();
 }
 
 $target = escape($_POST['level_up'], true);
+$amount = (int)$_POST['amount'];
+
+if($amount < 1 || $amount > 1000){
+	echo 0;
+	die();
+}
+
 $user = userDetails($target);
 
 if(empty($user) || !useLevel() || !canRankUser($user)){
@@ -20,11 +27,11 @@ if(empty($exp)){
 	die();
 }
 
-$mysqli->query("UPDATE boom_users SET user_level = user_level + 1 WHERE user_id = '{$user['user_id']}'");
+$mysqli->query("UPDATE boom_users SET user_level = user_level + {$amount} WHERE user_id = '{$user['user_id']}'");
 $mysqli->query("UPDATE boom_exp SET exp_current = 0 WHERE uid = '{$user['user_id']}'");
 
 clearNotifyAction($user['user_id'], 'level');
-boomNotify('level', array('target'=> $user['user_id'], 'source'=> 'level', 'icon'=> 'level', 'custom'=> $user['user_level'] + 1));
+boomNotify('level', array('target'=> $user['user_id'], 'source'=> 'level', 'icon'=> 'level', 'custom'=> $user['user_level'] + $amount));
 redisUpdateUser($user['user_id']);
 
 echo 1;
