5219f87fcd77ca0389bbfc72af980b92bb9abcf0
hearts-byte <tlaqywtalt@gmail.com>
2026-08-18 02:21:15 +0300

    Add files via upload

--- system/database.php
+++ system/database.php
@@ -2,8 +2,8 @@
 // base system prefix
 define('BOOM_PREFIX', 'tc_');
 
-// optional base domain
-define('BOOM_DOMAIN', '');
+// optional base domain (ضع رابط موقعك على Railway)
+define('BOOM_DOMAIN', 'https://mlok1.up.railway.app/');
 
 // default redis configuration
 define('REDIS_IP', '127.0.0.1');
@@ -11,17 +11,17 @@ define('REDIS_PORT', 6379);
 define('REDIS_TIMEOUT', 0.2);
 define('REDIS_PASS', '');
 
-// you can edit these lines to configure new setting for your chat
-define('BOOM_DHOST', '');
-define('BOOM_DUSER', '');
-define('BOOM_DPASS', '');
-define('BOOM_DNAME', '');
+// database configuration
+define('BOOM_DHOST', 'mysql.railway.internal');
+define('BOOM_DUSER', 'root');
+define('BOOM_DPASS', 'KuBNPPVJIhAboFklmZwymrhTdYsKPTAB');
+define('BOOM_DNAME', 'railway');
 
 // base system main path do not modify
 define('BOOM_PATH', dirname(__DIR__));
 
 // do not modify those variables
-define('BOOM_CRYPT', '');
-define('BOOM_INSTALL', 0);
+define('BOOM_CRYPT', 'tc_fixed_secret_key_9988');
+define('BOOM_INSTALL', 1);
 define('BOOM', 1);
 ?>
