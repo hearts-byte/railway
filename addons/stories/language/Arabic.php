517e69ed96b1cee34958a18b9c8cacb2bc280fbe
hearts-byte <tlaqywtalt@gmail.com>
2026-08-23 01:52:08 +0300

    Add files via upload

---
+++ addons/stories/language/Arabic.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined('IN_SCRIPT')) { die('Access Denied'); }
+
+$lang['stories_title']          = 'الستوريات';
+$lang['stories_add']            = 'إضافة ستوري';
+$lang['stories_my_story']       = 'إضافتك';
+$lang['stories_new_story']      = 'ستوري جديدة';
+$lang['stories_tab_text']       = 'نص';
+$lang['stories_tab_image']      = 'صورة';
+$lang['stories_tab_video']      = 'فيديو';
+$lang['stories_publish']        = 'نشر الستوري';
+$lang['stories_placeholder']    = 'اكتب شيئاً...';
+$lang['stories_paid_option']    = 'نشر مدفوع بالذهب';
+$lang['stories_views']          = 'مشاهدة';
+$lang['stories_viewers_title']  = 'المشاهدون';
+$lang['stories_reply_placeholder'] = 'أرسل رداً...';
+$lang['stories_send']           = 'إرسال';
+$lang['stories_err_login']      = 'يجب تسجيل الدخول أولاً';
+$lang['stories_err_empty_text'] = 'لا يمكن نشر ستوري نصية فارغة';
+$lang['stories_err_no_file']    = 'لم يتم إرفاق أي ملف';
+$lang['stories_err_gold']       = 'رصيد الذهب غير كافٍ لنشر هذه الستوري';
+$lang['stories_err_not_owner']  = 'هذه البيانات متاحة لصاحب الستوري فقط';
+$lang['stories_err_expired']    = 'الستوري غير موجودة أو انتهت';
