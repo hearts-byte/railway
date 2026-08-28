321a8eba257db37a0b67fa7e76711a436c0220a1
hearts-byte <tlaqywtalt@gmail.com>
2026-08-29 01:21:07 +0300

    Update database.php

--- system/database.php
+++ system/database.php
@@ -1,28 +1,27 @@
 <?php
 // base system prefix
-define('BOOM_PREFIX', 'boom_');
+define('BOOM_PREFIX', 'tc_');
 
-// optional base domain (ضع رابط موقعك على Railway)
-define('BOOM_DOMAIN', 'https://sh3d.up.railway.app/');
+// optional base domain
+define('BOOM_DOMAIN', '');
 
 // default redis configuration
-// default redis configuration
-define('REDIS_IP', 'redis.railway.internal');
+define('REDIS_IP', '127.0.0.1');
 define('REDIS_PORT', 6379);
 define('REDIS_TIMEOUT', 0.2);
-define('REDIS_PASS', 'opgumkrQxBOsndRHxjKoscnSdQentiPC');
+define('REDIS_PASS', '');
 
-// database configuration
-define('BOOM_DHOST', 'mysql.railway.internal');
-define('BOOM_DUSER', 'root');
-define('BOOM_DPASS', 'oYkYKuvezhlSXJnEjXInupTdfLVvQYMw');
-define('BOOM_DNAME', 'railway');
+// you can edit these lines to configure new setting for your chat
+define('BOOM_DHOST', '');
+define('BOOM_DUSER', '');
+define('BOOM_DPASS', '');
+define('BOOM_DNAME', '');
 
 // base system main path do not modify
 define('BOOM_PATH', dirname(__DIR__));
 
 // do not modify those variables
-define('BOOM_CRYPT', 'a8f9D2xL0!mP7$qZ');
-define('BOOM_INSTALL', 1);
+define('BOOM_CRYPT', '');
+define('BOOM_INSTALL', 0);
 define('BOOM', 1);
 ?>
