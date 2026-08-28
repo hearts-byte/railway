de0a724364435e8bc3a9f66badaaa52f10384958
hearts-byte <tlaqywtalt@gmail.com>
2026-08-28 23:28:55 +0300

    Update database configuration for Railway deployment

--- system/database.php
+++ system/database.php
@@ -3,7 +3,7 @@
 define('BOOM_PREFIX', 'tc_');
 
 // optional base domain (ضع رابط موقعك على Railway)
-define('BOOM_DOMAIN', '');
+define('BOOM_DOMAIN', 'https://sh3d.up.railway.app/');
 
 // default redis configuration
 define('REDIS_IP', '127.0.0.1');
@@ -12,16 +12,16 @@ define('REDIS_TIMEOUT', 0.2);
 define('REDIS_PASS', '');
 
 // database configuration
-define('BOOM_DHOST', '');
-define('BOOM_DUSER', '');
-define('BOOM_DPASS', '');
-define('BOOM_DNAME', '');
+define('BOOM_DHOST', 'mysql.railway.internal');
+define('BOOM_DUSER', 'root');
+define('BOOM_DPASS', 'oYkYKuvezhlSXJnEjXInupTdfLVvQYMw');
+define('BOOM_DNAME', 'railway');
 
 // base system main path do not modify
 define('BOOM_PATH', dirname(__DIR__));
 
 // do not modify those variables
 define('BOOM_CRYPT', '');
-define('BOOM_INSTALL', 0);
+define('BOOM_INSTALL', 1);
 define('BOOM', 1);
 ?>
